8347b8a18bc3fb7bc324f5a1eda2e34bd5265dd6
Drake Wilcox <drakewilcox@gmail.com>
2020-01-24 14:43:12 -0800

    add if statements to replace words with all the digits if the entered number includes 3, 2, or 1.

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,26 @@
 var newArray = []
 var beepBoop = function(number){
   var total = 0;
-  for( var currentNumber = 0; currentNumber < number; currentNumber ++){
+  for( var currentNumber = 0; currentNumber <= number; currentNumber ++){
     
     {currentNumber+1;}
     console.log(currentNumber)
-    newArray.push(currentNumber)
+    newArray.push(currentNumber + "<br>")
+    var output = currentNumber.toString();
+    if (output.includes ("3")){
+      newArray.splice(currentNumber, 1, "'I'm sorry, Dave. I'm afraid I can't do that.'<br>")
+    }
+    else if (output.includes("2")){
+      newArray.splice(currentNumber, 1, "'Boop!'<br>")
+    }
+    else if (output.includes("1")){
+      newArray.splice(currentNumber, 1, "'Beep!'<br>")
+    }
   }
   console.log(newArray);
-  return (newArray.join(' '));
-  // return currentNumber;
+  
+  
+ return (newArray.join(' '));
 }
 
 
