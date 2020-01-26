4f0844487c6e5ddbb6b8c9b4929e65ea372a0075
Drake Wilcox <drakewilcox@gmail.com>
2020-01-26 15:46:27 -0800

    add basic styles to css/styles.css to include a header and gold button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,25 @@
 // BUSINESS LOGIC 
-// var number 
+
 
 var beepBoop = function(number){
   var newArray = []
   var total = 0;
   for( var currentNumber = 0; currentNumber <= number; currentNumber ++){
     
-  
-    // console.log(currentNumber)
+
     newArray.push(currentNumber + "<br>")
     var output = currentNumber.toString();
     if (output.includes ("3")){
-      newArray.splice(currentNumber, 1, "I'm sorry, Dave. I'm afraid I can't do that.'<br>")
+      newArray.splice(currentNumber, 1, "'I'm sorry, Dave. I'm afraid I can't do that.'<br>")
     }
     else if (output.includes("2")){
-      newArray.splice(currentNumber, 1, "Boop!<br>")
+      newArray.splice(currentNumber, 1, "'Boop!'<br>")
     }
     else if (output.includes("1")){
       newArray.splice(currentNumber, 1, "'Beep!'<br>")
     }
   }
-  // console.log(newArray);
+
   
   
   return (newArray).join("");
